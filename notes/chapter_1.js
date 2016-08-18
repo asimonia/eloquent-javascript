df9f227673649956e1e203e9f08e64e80ded5ec7
asimonia <alex.simonian@gmail.com>
2016-08-18 18:48:47 -0500

    Added Chapter 1 notes

---
+++ notes/chapter_1.js
@@ -0,0 +1,29 @@
+// Values, types, operators
+// Key Concepts & Questions
+
+/*  
+(1) What are the 6 types in javascript?  What is a type and value?
+(2) How is js weakly typed?
+(3) Talk about logical, comparison, arithmetic, and ternary operators operators.
+*/
+
+
+// Values are made up of bits and bytes
+// Types are types of values. There are six.
+// number, string, boolean, object, function, undefined
+// Javascript is weakly typed, so you can do weird things like "5" + 1 returns 4
+
+
+// Operators are symbols that perform some function on the operands
+// Comparisons, arithmetic
+// && (and)		|| (or)		!= (not)	(short-circuit evaluation)
+// && returns true only if both operands are true
+// || returns true if either or operands are true
+// ===  (equal to value and type)  (use three character comparisions)
+// NaN is not a number
+
+// Ternary operator (conditional)
+// True ? 1 : 2			// returns 1
+// False ? 10 : 100		// returns 100
+
+
