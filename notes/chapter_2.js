f448ad6c67386495f1a3ba56315bc149fe2ac59a
asimonia <alex.simonian@gmail.com>
2016-08-18 21:54:36 -0500

    Add 3rd chapter

---
+++ notes/chapter_2.js
@@ -0,0 +1,32 @@
+// Key Concepts & Questions
+
+/*  
+(1) Explain statements, expressions, varaibles, keywords
+(2) What is Branching?
+(3) What is Looping?
+*/
+
+
+// Expressions are content to return a value
+// Statements are lines of code, ended with ;
+// Variables _grasp_ values
+// Environment contains the variables that exist
+// Keywords are reserved and have special meaning, like
+// Execute function
+
+break case catch class const continue debugger
+default delete do else enum export extends false
+finally for function if implements import in
+instanceof interface let new null package private
+protected public return static super switch this
+throw true try typeof var void while with yield
+
+
+var ten = 10;
+console.log(ten * ten);			// output to the console
+
+// Functions are called -> return values
+
+// Branching: conditional code : if, elseif, else ; switch-case
+// Looping: do, while, for loops, breaking out
+
